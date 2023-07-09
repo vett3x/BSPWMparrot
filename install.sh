0c4e79e0c7b2a99183176b5d97d8dbcba074497a
Vett3x <16060633+vett3x@users.noreply.github.com>
2023-07-09 19:21:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,9 +173,13 @@ go build
 cd /usr/share/
 sudo git clone https://github.com/danielmiessler/SecLists.git
 
+# Instalando Gotop
+cd ~/Herramientas
+git clone --depth 1 https://github.com/cjbassi/gotop
+sudo ~/Herramientas/gotop/scripts/download.sh
+
 # Fix archivos inseguros de zsh
 sudo chown -R root:root /usr/local/share/zsh/site-functions
-
 
 # Removiendo Repositorio
 
